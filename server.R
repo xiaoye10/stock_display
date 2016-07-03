2b40f4ad83cc79b0188b8a7c8c5012e165593bd9
xiaoye10 <1192941329@qq.com>
2016-07-03 16:37:12 +0800

    Create server.R

---
+++ server.R
@@ -0,0 +1,17 @@
+library(shiny)
+library(quantmod)
+shinyServer(function(input,output){
+  dataInput <- reactive({
+    getSymbols(input$symb, src = "yahoo",
+               from = input$dates[1],
+               to = input$dates[2],
+               auto.assign = FALSE)
+  })
+ 
+  output$plot <- renderPlot({
+    chartSeries(dataInput(), theme = chartTheme("white"),
+                type = "line",log.scale = input$log, TA = NULL)
+    addADX()
+  })
+}
+)
